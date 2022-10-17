db54941681bc24ec0cc1017a0839d55c514c4239
BMcDavitt <bill.mcdavitt@yahoo.com>
2022-10-17 17:57:20 -0400

    new project cards

--- data.js
+++ data.js
@@ -1,28 +1,46 @@
 const pageData = [
-    {
-        name:"BoostX",
-        image:"boostx.png",
-        description:"This is an exmaple of a website built based on the below Figma design.  This is a fully responsive webpage deveoped using react.",
-        link:"https://bmcdavitt.github.io/boostx/",
-        source:"https://github.com/BMcdavitt/boostx/tree/master",
-        specification:"https://www.figma.com/file/JDVVrItlNBFp4jUC5hGRSX/BoostX?node-id=13%3A858",
-        specificationText:"View Figma File",
-    },
-    {
-        name:"Simple Image Annotation",
-        image:"kovana.png",
-        description:"This project uses the Kovana library to create a simple image annotation tool.  This implements both mouse and touch support.",
-        link:"https://bmcdavitt.github.io/mypage/",
-        source:"https://github.com/BMcdavitt/mypage",
-    },
-    {
-        name:"React Blackjack",
-        image:"blackjack.png",
-        description:"After learning react I created a simple blackjack game to reinforce my new skills.",
-        link:"https://bmcdavitt.github.io/reactBlackjack/",
-        source:"https://github.com/BMcdavitt/reactBlackjack/tree/master",
-    },
-
+  {
+    name: 'BoostX',
+    image: 'boostx.png',
+    description:
+      'This is an exmaple of a website built based on the below Figma design.  This is a fully responsive webpage deveoped using react.',
+    link: 'https://bmcdavitt.github.io/boostx/',
+    source: 'https://github.com/BMcdavitt/boostx/tree/master',
+    specification:
+      'https://www.figma.com/file/JDVVrItlNBFp4jUC5hGRSX/BoostX?node-id=13%3A858',
+    specificationText: 'View Figma File',
+  },
+  {
+    name: 'Drawing Tool',
+    image: 'konva.png',
+    description:
+      'This project uses the Konva library to create a simple drawing tool.  This implements both mouse and touch support.',
+    link: 'https://bmcdavitt.github.io/annotator/',
+    source: 'https://github.com/BMcdavitt/annotator',
+  },
+  {
+    name: 'React Blackjack',
+    image: 'blackjack.png',
+    description:
+      'After learning react I created a simple blackjack game to reinforce my new skills.',
+    link: 'https://bmcdavitt.github.io/reactBlackjack/',
+    source: 'https://github.com/BMcdavitt/reactBlackjack/tree/master',
+  },
+  {
+    name: 'Quizzical',
+    image: 'quizzical.png',
+    description:
+      'A simple quiz game.  This project makes use of the Open Trivia Database RestfulAPI ',
+    link: 'https://bmcdavitt.github.io/reactQuiz/',
+    source: 'https://github.com/BMcdavitt/reactQuiz/tree/master/src',
+  },
+  {
+    name: 'Apex Charts using Weather API',
+    image: 'apex.png',
+    description:
+      'This is a simple proof of concept using ApexCharts.js to display graphical information obtained from the openweathermap.org API',
+    link: 'https://bmcdavitt.github.io/apexDemo/',
+    source: 'https://github.com/BMcdavitt/apexDemo/tree/master',
+  },
 ]
-module.exports = {pageData}
-
+module.exports = { pageData }
