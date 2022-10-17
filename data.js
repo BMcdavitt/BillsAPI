ccb2ff1c7cb41ba39490a9baef0a951799c420a9
BMcDavitt <bill.mcdavitt@yahoo.com>
2022-10-17 18:12:10 -0400

    add Front End Mentor and 100 Day CSS cards

--- data.js
+++ data.js
@@ -1,46 +1,163 @@
 const pageData = [
   {
-    name: 'BoostX',
-    image: 'boostx.png',
-    description:
-      'This is an exmaple of a website built based on the below Figma design.  This is a fully responsive webpage deveoped using react.',
-    link: 'https://bmcdavitt.github.io/boostx/',
-    source: 'https://github.com/BMcdavitt/boostx/tree/master',
-    specification:
-      'https://www.figma.com/file/JDVVrItlNBFp4jUC5hGRSX/BoostX?node-id=13%3A858',
-    specificationText: 'View Figma File',
+    cardType: 'ProjectCard',
+    cardData: [
+      {
+        name: 'BoostX',
+        image: 'boostx.png',
+        description:
+          'This is an exmaple of a website built based on the below Figma design.  This is a fully responsive webpage deveoped using react.',
+        link: 'https://bmcdavitt.github.io/boostx/',
+        source: 'https://github.com/BMcdavitt/boostx/tree/master',
+        specification:
+          'https://www.figma.com/file/JDVVrItlNBFp4jUC5hGRSX/BoostX?node-id=13%3A858',
+        specificationText: 'View Figma File',
+      },
+      {
+        name: 'Drawing Tool',
+        image: 'konva.png',
+        description:
+          'This project uses the Konva library to create a simple drawing tool.  This implements both mouse and touch support.',
+        link: 'https://bmcdavitt.github.io/annotator/',
+        source: 'https://github.com/BMcdavitt/annotator',
+      },
+      {
+        name: 'React Blackjack',
+        image: 'blackjack.png',
+        description:
+          'After learning react I created a simple blackjack game to reinforce my new skills.',
+        link: 'https://bmcdavitt.github.io/reactBlackjack/',
+        source: 'https://github.com/BMcdavitt/reactBlackjack/tree/master',
+      },
+      {
+        name: 'Quizzical',
+        image: 'quizzical.png',
+        description:
+          'A simple quiz game.  This project makes use of the Open Trivia Database RestfulAPI ',
+        link: 'https://bmcdavitt.github.io/reactQuiz/',
+        source: 'https://github.com/BMcdavitt/reactQuiz/tree/master/src',
+      },
+      {
+        name: 'Apex Charts using Weather API',
+        image: 'apex.png',
+        description:
+          'This is a simple proof of concept using ApexCharts.js to display graphical information obtained from the openweathermap.org API',
+        link: 'https://bmcdavitt.github.io/apexDemo/',
+        source: 'https://github.com/BMcdavitt/apexDemo/tree/master',
+      },
+    ],
   },
   {
-    name: 'Drawing Tool',
-    image: 'konva.png',
-    description:
-      'This project uses the Konva library to create a simple drawing tool.  This implements both mouse and touch support.',
-    link: 'https://bmcdavitt.github.io/annotator/',
-    source: 'https://github.com/BMcdavitt/annotator',
+    cardType: 'FrontEndMentorCard',
+    cardData: [
+      {
+        name: 'SinglePriceGridComponent',
+        image: 'SinglePriceGridComponent.png',
+        title: 'Single Price Grid Component',
+        link: 'https://bmcdavitt.github.io/femSinglePriceGridComponent/',
+      },
+      {
+        name: 'BaseApparelComingSoon',
+        image: 'BaseApparelComingSoon.png',
+        title: 'Base Apparel Coming Soon',
+        link: 'https://bmcdavitt.github.io/femBaseApparelComingSoon/',
+      },
+      {
+        name: 'Four Card Feature Component',
+        image: 'FourCardFeature.png',
+        title: 'Four Card Feature Component',
+        link: 'https://bmcdavitt.github.io/femFourCardFeatureSection/',
+      },
+      {
+        name: 'Article Preview Component',
+        image: 'ArticlePreview.png',
+        title: 'Article Preview Component',
+        link: 'https://bmcdavitt.github.io/femArticlePreviewComponent/',
+      },
+      {
+        name: 'Social Proof Section',
+        image: 'SocialProofSection.png',
+        title: 'Social Proof Section',
+        link: 'https://bmcdavitt.github.io/femSocialProofSectionMaster/',
+      },
+      {
+        name: 'FAQ Accordion Card',
+        image: 'FAQAccordion.png',
+        title: 'FAQ Accordion Card',
+        link: 'https://bmcdavitt.github.io/femFaqAccordionCard/',
+      },
+      {
+        name: 'ProfileCard',
+        image: 'ProfileCard.png',
+        title: 'Profile Card',
+        link: 'https://bmcdavitt.github.io/femProfileCard/',
+      },
+      {
+        name: 'InteractiveCardDetailsform',
+        image: 'InteractiveCardDetailsForm.png',
+        title: 'Interactive Card Details form',
+        link: 'https://bmcdavitt.github.io/femInteractiveCardDetails/',
+      },
+      {
+        name: 'MyResponsiveDesign',
+        image: 'MyResponsiveDesign.png',
+        title: 'My Responsive Design',
+        link: 'https://bmcdavitt.github.io/femStatsPreviewCard/',
+      },
+      {
+        name: 'ResponsiveOrderSummary',
+        image: 'ResponsiveOrderSummary.png',
+        title: 'Responsive Order Summary',
+        link: 'https://bmcdavitt.github.io/femOrderSummary/',
+      },
+      {
+        name: 'ThreeColumnResponsiveDisplay',
+        image: 'ThreeColumnResponsiveDisplay.png',
+        title: 'Three Column Responsive Display',
+        link: 'https://bmcdavitt.github.io/remThreeColPreviewCard/',
+      },
+      {
+        name: 'NFTPreviewCard',
+        image: 'NFTPreviewCard.png',
+        title: 'NFT Preview Card',
+        link: 'https://bmcdavitt.github.io/femNftPreview/',
+      },
+      {
+        name: 'InteractiveRatingComponent',
+        image: 'InteractiveRatingComponent.png',
+        title: 'Interactive Rating Component',
+        link: 'https://bmcdavitt.github.io/femRatingComponent/',
+      },
+      {
+        name: 'ResponsiveProductCard',
+        image: 'ResponsiveProductCard.png',
+        title: 'Responsive Product Card',
+        link: 'https://bmcdavitt.github.io/femProductCard/',
+      },
+      {
+        name: 'FrontEndMentorStarterChallenge',
+        image: 'FrontEndMentorStarterChallenge.png',
+        title: 'Front End Mentor Starter Challenge',
+        link: 'https://bmcdavitt.github.io/femQrCode/',
+      },
+    ],
   },
   {
-    name: 'React Blackjack',
-    image: 'blackjack.png',
-    description:
-      'After learning react I created a simple blackjack game to reinforce my new skills.',
-    link: 'https://bmcdavitt.github.io/reactBlackjack/',
-    source: 'https://github.com/BMcdavitt/reactBlackjack/tree/master',
-  },
-  {
-    name: 'Quizzical',
-    image: 'quizzical.png',
-    description:
-      'A simple quiz game.  This project makes use of the Open Trivia Database RestfulAPI ',
-    link: 'https://bmcdavitt.github.io/reactQuiz/',
-    source: 'https://github.com/BMcdavitt/reactQuiz/tree/master/src',
-  },
-  {
-    name: 'Apex Charts using Weather API',
-    image: 'apex.png',
-    description:
-      'This is a simple proof of concept using ApexCharts.js to display graphical information obtained from the openweathermap.org API',
-    link: 'https://bmcdavitt.github.io/apexDemo/',
-    source: 'https://github.com/BMcdavitt/apexDemo/tree/master',
+    cardType: 'OneHundredDayCssCard',
+    cardData: [
+      { src: 'poVYKqK' },
+      { src: 'gOzEaRe' },
+      { src: 'jOxXEEy' },
+      { src: 'GRdBvWO' },
+      { src: 'jOxzpre' },
+      { src: 'eYrVGrX' },
+      { src: 'MWGrjav' },
+      { src: 'NWMvpjM' },
+      { src: 'QWrEOyL' },
+      { src: 'bGMpoQy' },
+      { src: 'VwxedqB' },
+    ],
   },
 ]
+
 module.exports = { pageData }
